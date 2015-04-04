927d896b257281163f7d8bdaaf961523da75bb83
srt32 <simon.taranto@gmail.com>
2015-04-03 18:29:22 -0700

    Allow user to remove item from list

--- app/src/main/java/com/simontaranto/simpletodo/MainActivity.java
+++ app/src/main/java/com/simontaranto/simpletodo/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -28,6 +29,7 @@ public class MainActivity extends ActionBarActivity {
         lvItems.setAdapter(itemsAdapter);
         items.add("First Item");
         items.add("Second Item");
+        setupListViewListener();
     }
 
 
@@ -58,5 +60,19 @@ public class MainActivity extends ActionBarActivity {
         String itemText = etNewItem.getText().toString();
         itemsAdapter.add(itemText);
         etNewItem.setText("");
+    }
+
+    public void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        items.remove(pos);
+                        itemsAdapter.notifyDataSetChanged();
+                        return true;
+                    }
+                }
+        );
     }
 }
